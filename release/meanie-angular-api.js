1f3faf98a663056a6ed647153443f868204dc528
Adam Buczynski <adambuczynski@gmail.com>
2015-07-31 12:52:01 +1200

    Add duplicate requests filter

--- release/meanie-angular-api.js
+++ release/meanie-angular-api.js
@@ -1,5 +1,5 @@
 /**
- * meanie-angular-api - v0.4.7 - 31-6-2015
+ * meanie-angular-api - v0.4.8 - 31-6-2015
  * https://github.com/meanie/angular-api
  *
  * Copyright (c) 2015 Adam Buczynski <me@adambuczynski.com>
@@ -314,6 +314,106 @@ angular.module('Api.Service', [
     return Api;
   }];
 });
+
+})(window, window.angular);
+
+(function(window, angular, undefined) {'use strict';
+
+/**
+ * Module definition and dependencies
+ */
+angular.module('Api.DuplicateRequestsFilter.Decorator', [])
+
+/**
+ * Config
+ */
+.config(['$provide', function($provide) {
+
+  /**
+   * Decorator for the $http service
+   */
+  $provide.decorator('$http', ['$delegate', '$q', function($delegate, $q) {
+
+    /**
+     * Pending requests and local $http var for natural reference
+     */
+    var pendingRequests = {};
+    var $http = $delegate;
+
+    /**
+     * Hash generator
+     */
+    function hash(str) {
+      var h = 0;
+      var strlen = str.length;
+      if (strlen === 0) {
+        return h;
+      }
+      for (var i = 0, n; i < strlen; ++i) {
+        n = str.charCodeAt(i);
+        h = ((h << 5) - h) + n;
+        h = h & h;
+      }
+      return h >>> 0;
+    }
+
+    /**
+     * Helper to generate a unique identifier for a request
+     */
+    function getRequestIdentifier(config) {
+      var str = config.method + config.url;
+      if (config.data && typeof config.data === 'object') {
+        str += angular.toJson(config.data);
+      }
+      return hash(str);
+    }
+
+    /**
+     * Modified $http service
+     */
+    var $duplicateRequestsFilter = function(config) {
+
+      //Ignore for this request?
+      if (config.ignoreDuplicateRequest) {
+        return $http(config);
+      }
+
+      //Get unique request identifier
+      var identifier = getRequestIdentifier(config);
+
+      //Check if such a request is pending already
+      if (pendingRequests[identifier]) {
+        if (config.rejectDuplicateRequest) {
+          return $q.reject({
+            data: '',
+            headers: {},
+            status: config.rejectDuplicateStatusCode || 400,
+            config: config
+          });
+        }
+        return pendingRequests[identifier];
+      }
+
+      //Create promise using $http and make sure it's reset when resolved
+      pendingRequests[identifier] = $http(config).finally(function() {
+        delete pendingRequests[identifier];
+      });
+
+      //Return promise
+      return pendingRequests[identifier];
+    };
+
+    //Map rest of methods
+    Object.keys($http).filter(function(key) {
+      return (typeof $http[key] === 'function');
+    }).forEach(function(key) {
+      $duplicateRequestsFilter[key] = $http[key];
+    });
+
+    //Return it
+    return $duplicateRequestsFilter;
+  }]);
+}]);
 
 })(window, window.angular);
 
