5fb64de0d521b42d9b201cac83d53244018ec598
Adam Buczynski <adambuczynski@gmail.com>
2015-09-04 14:01:50 +1200

    Bump version to 0.5.3

--- release/meanie-angular-api.min.js
+++ release/meanie-angular-api.min.js
@@ -1,5 +1,5 @@
 /**
- * meanie-angular-api - v0.5.2 - 4-8-2015
+ * meanie-angular-api - v0.5.3 - 4-8-2015
  * https://github.com/meanie/angular-api
  *
  * Copyright (c) 2015 Adam Buczynski <me@adambuczynski.com>
