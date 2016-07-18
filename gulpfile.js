c57608cbb5cf08872e3a0778a057edda2837ae55
Adam Buczynski <adambuczynski@gmail.com>
2016-07-18 20:11:21 +1200

    No need for version/date in banner

--- gulpfile.js
+++ gulpfile.js
@@ -62,14 +62,11 @@ function bannerWrapper() {
   //Get date and author
   let today = new Date();
   let author = pkg.author.name + ' <' + pkg.author.email + '>';
-  let date = today.getDate() + '-' +
-             today.getMonth() + '-' +
-             today.getFullYear();
 
   //Format banner
   let banner =
     '/**\n' +
-    ' * ' + pkg.name + ' - v' + pkg.version + ' - ' + date + '\n' +
+    ' * ' + pkg.name +
     ' * ' + pkg.homepage + '\n' +
     ' *\n' +
     ' * Copyright (c) ' + today.getFullYear() + ' ' + author + '\n' +
