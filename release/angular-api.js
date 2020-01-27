22021922ee4e427a5adc0fa2b48a6954f2d5d141
Adam Reis <adam@reis.nz>
2020-01-27 14:30:28 +1300

    3.3.0

--- release/angular-api.js
+++ release/angular-api.js
@@ -1,7 +1,7 @@
 /**
  * @meanie/angular-api * https://github.com/meanie/angular-api
  *
- * Copyright (c) 2019 Adam Reis <adam@reis.nz>
+ * Copyright (c) 2020 Adam Reis <adam@reis.nz>
  * License: MIT
  */
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
